cd0bfeaee35e221ef056f969ec512a3fc13130e2
arkiver <arkiver@protonmail.com>
2020-12-31 03:12:52 +0100

    Version flashdomains. Tracker ID to flashdomains.

--- pipeline.py
+++ pipeline.py
@@ -56,9 +56,9 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20201231.01'
+VERSION = '20201231.02'
 USER_AGENT = 'Archive Team'
-TRACKER_ID = 'domains-flash'
+TRACKER_ID = 'flashdomains'
 TRACKER_HOST = 'trackerproxy.archiveteam.org'
 
 
